6e894b25df2f9b325039e4a672572a336bd0ea6e
Ole Nielsen <Ole.Moller.Nielsen@gmail.com>
2021-08-03 14:53:57 +1000

    PEP8 compliance

--- rank_skills.py
+++ rank_skills.py
@@ -1,4 +1,5 @@
-"""Read CSV file from 2021 Digital Science Maturity and Skills Survey and rank skills by importance.
+"""Read CSV file from 2021 Digital Science Maturity and Skills Survey
+   and rank skills by importance.
 
 Example:
 python rank_skills.py "Digital Science Maturity and Skills Deep Dive.csv"
@@ -6,10 +7,14 @@ python rank_skills.py "Digital Science Maturity and Skills Deep Dive.csv"
 Ole Nielsen - 2021
 """
 
-import csv, sys, pandas, numpy
-from skills_analysis_helpers import extract_data, SFIA_skills, SFIA_abbreviations, response_values, sort_dictionary_by_value
+import csv
+import sys
+import pandas
+import numpy
 
-   
+from skills_analysis_helpers import extract_data, SFIA_skills,\
+    SFIA_abbreviations, response_values, sort_dictionary_by_value
+
 # Get filename
 if len(sys.argv) != 2:
     msg = 'Filename with survey data must be supplied as command line argument'
@@ -18,50 +23,50 @@ filename = sys.argv[1]
 
 
 # Read data from CSV file
-# There are three responses for each skill: 
-# - How much it is needed, 
-# - How well we can access it and 
-# - How sustainable is it. 
+# There are three responses for each skill:
+# - How much it is needed,
+# - How well we can access it and
+# - How sustainable is it.
 # Therefore we read three columns for each skill.
 
 skills_dict = {}
 dataframe = pandas.read_csv(filename)
 
-number_of_responses = -1 # Collect number of responses and check that it is the same across all columns.
+number_of_responses = -1  # Flag keeping track of columns
 for skill in SFIA_abbreviations:
     # Collect responses for each skill
-    
+
     # Find columns with responses for this skill.
     # This is done through linear searching, but data is small
     # so it doesn't pose a performance issue.
-    
+
     additional_columns_to_collect = 0
     for item in dataframe.items():
         if additional_columns_to_collect > 0:
             # Collect remaining responses for current skill
-            
+
             if additional_columns_to_collect == 2:
                 d['ACCESS'] = extract_data('ACCESS', item)
-                
+
             if additional_columns_to_collect == 1:
-                d['SUSTAIN'] = extract_data('SUSTAIN', item)           
-                
+                d['SUSTAIN'] = extract_data('SUSTAIN', item)
+
             additional_columns_to_collect -= 1
-                    
+
         if skill in item[0]:
             # Collect first response for this skill
             additional_columns_to_collect = 2
-            
+
             d = skills_dict[skill] = {}   # Create new entry
             d['NEED'] = extract_data('NEED', item)
-            
-            # Record number of responses
+
+            # Record number of responses and test that it is the same across
+            # all columns.
             if number_of_responses == -1:
                 number_of_responses = len(d['NEED'])
             else:
                 msg = 'Number of responses were not the same across this data'
                 assert number_of_responses == len(d['NEED']), msg
-
 
 
 # Convert responses to numerical values
@@ -70,18 +75,20 @@ for skill in SFIA_abbreviations:
     responses[skill] = {}  # Create new entry for this skill
     skill_response = skills_dict[skill]  # Responses for this skill
     for key in skill_response:
-        responses[skill][key] = numpy.zeros(number_of_responses)    
+        responses[skill][key] = numpy.zeros(number_of_responses)
         for i, response in enumerate(skill_response[key]):
 
-            val = response_values[str(response)]  # Turn e.g. nan into a string to index structure.
+            # Make sure all indices are strings and get value
+            # from response values
+            val = response_values[str(response)]
             responses[skill][key][i] = val
-        
+
 
 # Calculate skills gaps (G) using the formula
 #
 # G = avg(N - min(A, S))
-# 
-# where 
+#
+# where
 # G is the skills gap
 # N is how much it is needed
 # A is how much access we have
@@ -94,51 +101,43 @@ for skill in SFIA_abbreviations:
     response = responses[skill]
     N = response['NEED']
     A = response['ACCESS']
-    S = response['SUSTAIN']        
-    
-    # Remove "N/A" and Don't Know    
-    N = N[~numpy.isnan(N)] 
-    A = A[~numpy.isnan(A)] 
-    S = S[~numpy.isnan(S)]    
-    
+    S = response['SUSTAIN']
+
+    # Remove "N/A" and Don't Know
+    N = N[~numpy.isnan(N)]
+    A = A[~numpy.isnan(A)]
+    S = S[~numpy.isnan(S)]
+
     # Find the mean values of each metric
-    N = numpy.mean(N)    
-    A = numpy.mean(A)    
-    S = numpy.mean(S)            
-                
+    N = numpy.mean(N)
+    A = numpy.mean(A)
+    S = numpy.mean(S)
+
     # Calculate Gap
     G = N - min(A, S)
-     
+
     # Save the values
     skills_gap[skill] = G
-    needs[skill] = N    
-
-
+    needs[skill] = N
 
 # Print out skills sorted by need only
-L = sort_dictionary_by_value(needs)     
-print()  
-print('------------------------------') 
+L = sort_dictionary_by_value(needs)
+print()
+print('------------------------------')
 print('Skills sorted by average need:')
-print('------------------------------') 
+print('------------------------------')
 for item in L[::-1]:  # Reverse order
     skill = item[0]
     weight = item[1]
     print('%.2f: %s (%s)' % (weight, SFIA_skills[skill], skill))
-    
+
 # Print out skills sorted by gap
 L = sort_dictionary_by_value(skills_gap)
 print()
-print('-----------------------------')     
+print('-----------------------------')
 print('Skills sorted by average gap:')
-print('-----------------------------') 
+print('-----------------------------')
 for item in L[::-1]:  # Reverse order
     skill = item[0]
     weight = item[1]
     print('%.2f: %s (%s)' % (weight, SFIA_skills[skill], skill))
-
-
-
-    
-
-    
--- skills_analysis_helpers.py
+++ skills_analysis_helpers.py
@@ -1,12 +1,11 @@
-"""Auxiliary functions and definitions for extracting and analysing skills from 
-GA Digital Science Maturity and Skills Deep Dive 2021
+"""Auxiliary functions and definitions for extracting and analysing skills
+   from GA Digital Science Maturity and Skills Deep Dive 2021
 """
 
 from numpy import nan
 
 
-
-SFIA_skills = {'IRMG': 'Information governance', 
+SFIA_skills = {'IRMG': 'Information governance',
                'SCTY': 'Information security',
                'INAN': 'Analytics',
                'VISL': 'Data visualisation',
@@ -28,34 +27,36 @@ SFIA_skills = {'IRMG': 'Information governance',
                'NUMA': 'Numerical Analysis',
                'HPCC': 'High Performance Computing',
                'DATS': 'Machine Learning and Data Science'}
-               
-SFIA_abbreviations = SFIA_skills.keys()               
-        
+
+SFIA_abbreviations = SFIA_skills.keys()
+
 response_values = {"Strongly Disagree": -2,
-                   "Disagree": -1, 
+                   "Disagree": -1,
                    "Neither agree nor disagree": 0,
                    "Agree": 1,
                    "Strongly Agree": 2,
                    "Don't Know": nan,
                    "nan": nan}  # IEEE value for Not A Number
-                
-        
+
+
 def extract_data(field, item):
     """Helper function to extract pandas data
     """
-    
+
     data = item[1:][0]  # Pull out data from tuple
-    assert(field.lower() in data[0]) # Check that the right keyword is in the header             
+
+    # Check that the right keyword is in the header
+    msg = 'Did not find expected keyword %s in %s' % (field, data[0])
+    assert field.lower() in data[0], msg
+
     return(list(data[1:]))  # Return data as a list
+
 
 def sort_dictionary_by_value(D):
     """Sort dictionary of the form key: value
     """
-    
+
     # Sort skills by gap (Schwarzian Transform)
     L = [(x[1], x) for x in D.items()]
     L.sort()
     return([(x[1][0], x[1][1]) for x in L])
-    
-
-    
